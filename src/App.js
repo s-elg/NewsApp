f594e7f97c4c08f2ce1bdb9c974063490f0f3e1a
sumeyye elgin <sumeyyeelgin@hotmail.com>
2024-07-24 09:53:52 +0300

    flatlist component tanımı yapıldı

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+//FlatList: herhangi bir şekilde veri görüntülenmesi yapacaksak bu componenti kullanırız
+//çalışma mantığı: uygulamaya 2000 tane veri akıyor ancak bizim ekranda göstereceğimiz 5 tane veri, bunu uygulamayı çok yormadan sağlayan bir component,
+//yani şu an ekranda 5 tane veri var, ekranı kaydırdığında da 5 tane veriyi hazırda tutuyo
+import React from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View>
+          <Text>News App</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
